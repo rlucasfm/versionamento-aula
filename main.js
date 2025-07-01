a2a900314043b962d0f93698962f0aab7b45f26b
Richard Lucas <richardlucasfm@gmail.com>
2025-07-01 20:00:56 -0300

    .

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const btn_pedido = document.getElementById("btn-pedido");
 
 btn_pedido.addEventListener("click", () => {
     alert("Pedido realizado com sucesso!");
+    console.log(`Número de visitantes: ${visitors}`);
 });
